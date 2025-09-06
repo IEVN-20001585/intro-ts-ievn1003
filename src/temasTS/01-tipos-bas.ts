93799d57500dbbe231731931a0bca26fb4d5ca36
IEVN-20001585 <20001585@alumnos.utleon.edu.mx>
2025-09-05 18:50:54 -0600

    Segundo commit

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola mundo")
+
+var num1=23;
+let num2=32;
+const num3=67;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+let sum = num1+num2+num3;
+console.log("La suma es:"+sum)
+
+let matricula: number | string;
+matricula = 1234532;
+matricula = 'yygusgu';
+console.log(matricula);
